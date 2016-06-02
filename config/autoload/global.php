4f8f47ed34b30b14a1854538a2f274d68e9cfede
Thomas Metois <thomas@netlor.fr>
2016-06-02 11:04:52 +0200

    Use phinx migration & multi-db doctrine

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,6 +11,7 @@
  * file.
  */
 return array(
+    // DOCTRINE CONF
     'doctrine' => array(
         'connection' => array(
             'orm_default' => array(
@@ -20,7 +21,21 @@ return array(
                     'port' => 3306,
                     'user' => 'user',
                     'password' => 'password',
-                    'dbname' => 'monarc',
+                    'dbname' => 'monarc_common',
+                    'charset' => 'utf8',
+                    'driverOptions' => array(
+                        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+                    ),
+                ),
+            ),
+            'orm_cli' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host' => 'localhost',
+                    'port' => 3306,
+                    'user' => 'root',
+                    'password' => 'password',
+                    'dbname' => 'monarc_cli',
                     'charset' => 'utf8',
                     'driverOptions' => array(
                         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
@@ -28,13 +43,62 @@ return array(
                 ),
             ),
         ),
-        'migrations_configuration' => array(
+        /*'migrations_configuration' => array(
             'orm_default' => array(
                 'name' => 'Monarc Migrations',
                 'directory' => __DIR__."/../../migrations",
                 'namespace' => 'Monarc\Migrations',
-                'table_name' => 'doctrine_migrations',
+                'table' => 'migrations',
+                'column' => 'version',
+            ),
+            'orm_cli' => array(
+                'name' => 'Monarc Common Migrations',
+                'directory' => __DIR__."/../../migrations",
+                'namespace' => 'MonarcCli\Migrations',
+                'table' => 'migrations',
+                'column' => 'version',
+            ),
+        ),*/
+        'entitymanager' => array(
+            'orm_default' => array(
+                'connection'    => 'orm_default',
+                'configuration' => 'orm_default'
+            ),
+            'orm_cli' => array(
+                'connection'    => 'orm_cli',
+                'configuration' => 'orm_cli',
+            ),
+        ),
+        'configuration' => array(
+            'orm_default' => array(
+                'metadata_cache'        => 'array',
+                'query_cache'           => 'array',
+                'result_cache'          => 'array',
+                'driver'                => 'orm_default', // This driver will be defined later
+                'generate_proxies'      => true,
+                'proxy_dir'             => 'data/DoctrineORMModule/Proxy',
+                'proxy_namespace'       => 'DoctrineORMModule\Proxy',
+                'filters'               => array(),
+                'datetime_functions'    => array(),
+                'string_functions'      => array(),
+                'numeric_functions'     => array(),
+                'second_level_cache'    => array(),
+            ),
+            'orm_cli' => array(
+                'metadata_cache'        => 'array',
+                'query_cache'           => 'array',
+                'result_cache'          => 'array',
+                'driver'                => 'orm_cli', // This driver will be defined later
+                'generate_proxies'      => true,
+                'proxy_dir'             => 'data/DoctrineORMModule/Proxy',
+                'proxy_namespace'       => 'DoctrineORMModule\Proxy',
+                'filters'               => array(),
+                'datetime_functions'    => array(),
+                'string_functions'      => array(),
+                'numeric_functions'     => array(),
+                'second_level_cache'    => array(),
             ),
         ),
     ),
+    // END DOCTRINE CONF
 );
