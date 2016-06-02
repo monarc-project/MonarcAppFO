4acfcb4cd115da5a80d26dbf49c0e1e16ccadae7
Guillaume Lesniak <guillaume@netlor.fr>
2016-06-02 12:16:03 +0200

    Update update-all with phinx migrations

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -11,11 +11,15 @@ pull_if_exists() {
 
 git pull
 
-php composer.phar update
+php composer.phar update -o
+
 
 pull_if_exists module/MonarcCore
 pull_if_exists module/MonarcBO
 pull_if_exists node_modules/ng_backoffice
+
+php ./vendor/robmorgan/phinx/bin/phinx migrate -c ./module/MonarcCore/migrations/phinx.php
+php ./vendor/robmorgan/phinx/bin/phinx migrate -c ./module/MonarcBO/migrations/phinx.php
 
 if [ -d node_modules/ng_backoffice ]; then
 	cd node_modules/ng_backoffice
