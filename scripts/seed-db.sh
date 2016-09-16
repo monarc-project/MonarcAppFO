0c01b23dbf72785a79077653b4dd3dab07e5f71a
Benjamin Ganivet <benjamin@netlor.fr>
2016-09-16 09:30:20 +0200

    fix phinx path

--- scripts/seed-db.sh
+++ scripts/seed-db.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ -d module/MonarcCore ]; then
-	php ./vendor/bin/phinx seed:run -c ./module/MonarcCore/migrations/phinx.php
+	php ./vendor/robmorgan/phinx/bin/phinx seed:run -c ./module/MonarcCore/migrations/phinx.php
 fi
 
 if [ -d module/MonarcBO ]; then
-	php ./vendor/bin/phinx seed:run -c ./module/MonarcBO/migrations/phinx.php
+	php ./vendor/robmorgan/phinx/bin/phinx seed:run -c ./module/MonarcBO/migrations/phinx.php
 fi
 
 if [ -d module/MonarcFO ]; then
-	php ./vendor/bin/phinx seed:run -c ./module/MonarcFO/migrations/phinx.php
+	php ./vendor/robmorgan/phinx/bin/phinx seed:run -c ./module/MonarcFO/migrations/phinx.php
 fi
 
