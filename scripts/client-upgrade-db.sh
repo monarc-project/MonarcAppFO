9f840400439fbb465df3d11b5b6ea299fa4ca1a2
Guillaume Lesniak <guillaume@netlor.fr>
2016-10-05 15:39:48 +0200

    scripts: Add client upgrade DB script

---
+++ scripts/client-upgrade-db.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Usage: ./scripts/client-upgrade-db.sh <module> <hostname> <user> <password> <db>
+# Example: ./scripts/client-upgrade-db.sh MonarcBO localhost root derp monarc_backoffice
+
+MODULE=$1
+SQLHOST=$2
+SQLUSER=$3
+SQLPASS=$4
+SQLBASE=$5
+
+cat <<EOF >/tmp/conf.tmp.php
+<?php
+return array(
+    'paths' => array(
+        'migrations' => 'module/$MODULE/migrations/db',
+        'seeds' => 'module/$MODULE/migrations/seeds',
+    ),
+    'environments' => array(
+        'default_migration_table' => 'phinxlog',
+        'default_database' => 'cli',
+        'cli' => array(
+            'adapter' => 'mysql',
+            'host' => '$SQLHOST',
+            'name' => '$SQLBASE',
+            'user' => '$SQLUSER',
+            'pass' => '$SQLPASS',
+            'port' => 3306,
+            'charset' => 'utf8',
+        ),
+    ),
+);
+
+EOF
+
+
+php ./vendor/robmorgan/phinx/bin/phinx migrate -c /tmp/conf.tmp.php
+rm /tmp/conf.tmp.php
