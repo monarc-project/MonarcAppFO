05572885ce077e9b83b0b8155dc9b40675ceb4df
Guillaume Lesniak <guillaume@netlor.fr>
2016-12-16 17:17:56 +0100

    ExtractTranslations: Use grunt from PATH or from node_modules, whichever is available

--- scripts/extract_translations.sh
+++ scripts/extract_translations.sh
@@ -2,11 +2,21 @@
 
 if [ -d node_modules/ng_backoffice ]; then
 	pushd node_modules/ng_backoffice
-	node_modules/.bin/grunt extract_translations
+
+	if [ -f node_modules/.bin/grunt ]; then
+		node_modules/.bin/grunt extract_translations
+	else
+		grunt extract_translations
+	fi
 fi
 
 
 if [ -d node_modules/ng_client ]; then
 	pushd node_modules/ng_client
-	node_modules/.bin/grunt extract_translations
+
+	if [ -f node_modules/.bin/grunt ]; then
+		node_modules/.bin/grunt extract_translations
+	else
+		grunt extract_translations
+	fi
 fi
