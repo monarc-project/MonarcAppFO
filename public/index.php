bb9e929d57bb1924005f1fc3bb84d7097865b667
Jerome De Almeida <jerome.dealmeida@vesperiagroup.com>
2017-02-01 16:40:43 +0100

    new license

--- public/index.php
+++ public/index.php
@@ -3,7 +3,6 @@
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
-phpinfo();
 chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
