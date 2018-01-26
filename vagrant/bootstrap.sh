f2cc98d802843ad73c6ec6578ef457b8833fcacd
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-01-26 07:25:49 +0100

    Replaced '...' by '…'.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -29,31 +29,31 @@ memory_limit=512M
 PHP_INI=/etc/php/7.1/apache2/php.ini
 
 
-echo -e "\n--- Installing MONARC FO... ---\n"
+echo -e "\n--- Installing MONARC FO… ---\n"
 
-echo -e "\n--- Updating packages list ---\n"
+echo -e "\n--- Updating packages list… ---\n"
 apt-get update
 
-echo -e "\n--- Install base packages ---\n"
+echo -e "\n--- Install base packages… ---\n"
 apt-get -y install vim zip unzip git gettext > /dev/null
 
-echo -e "\n--- Install MariaDB specific packages and settings ---\n"
+echo -e "\n--- Install MariaDB specific packages and settings… ---\n"
 echo "mysql-server mysql-server/root_password password $DBPASSWORD_ADMIN" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $DBPASSWORD_ADMIN" | debconf-set-selections
 apt-get -y install mariadb-server mariadb-client > /dev/null
 systemctl restart mariadb.service
 sleep 5
 
-echo -e "\n--- Installing PHP-specific packages ---\n"
+echo -e "\n--- Installing PHP-specific packages… ---\n"
 apt-get -y install php apache2 libapache2-mod-php php-curl php-gd php-mcrypt php-mysql php-pear php-apcu php-xml php-mbstring php-intl php-imagick php-zip > /dev/null
 
-echo -e "\n--- Configuring PHP ---\n"
+echo -e "\n--- Configuring PHP… ---\n"
 for key in upload_max_filesize post_max_size max_execution_time max_input_time memory_limit
 do
  sed -i "s/^\($key\).*/\1 = $(eval echo \${$key})/" $PHP_INI
 done
 
-echo -e "\n--- Enabling mod-rewrite and ssl ---\n"
+echo -e "\n--- Enabling mod-rewrite and ssl… ---\n"
 a2enmod rewrite > /dev/null 2>&1
 a2enmod ssl > /dev/null 2>&1
 
@@ -64,12 +64,12 @@ sudo sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
 #sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/apache2/php.ini
 #sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/apache2/php.ini
 
-echo -e "\n--- Setting up our MariaDB user for MONARC ---\n"
+echo -e "\n--- Setting up our MariaDB user for MONARC… ---\n"
 mysql -u root -p$DBPASSWORD_ADMIN -e "CREATE USER '$DBUSER_MONARC'@'localhost' IDENTIFIED BY '$DBPASSWORD_MONARC';"
 mysql -u root -p$DBPASSWORD_ADMIN -e "GRANT ALL PRIVILEGES ON * . * TO '$DBUSER_MONARC'@'localhost';"
 mysql -u root -p$DBPASSWORD_ADMIN -e "FLUSH PRIVILEGES;"
 
-echo -e "\n--- Installing composer... ---\n"
+echo -e "\n--- Installing composer… ---\n"
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "\nERROR: unable to install composer\n"
@@ -77,7 +77,7 @@ if [ $? -ne 0 ]; then
 fi
 composer self-update
 
-echo -e "\n--- Installing MONARC... ---\n"
+echo -e "\n--- Installing MONARC… ---\n"
 cd $PATH_TO_MONARC
 git config core.fileMode false
 if [ "$TAG" != '' ]; then
@@ -86,7 +86,7 @@ if [ "$TAG" != '' ]; then
     git checkout $TAG
 fi
 
-echo -e "\n--- Retrieving MONARC libraries... ---\n"
+echo -e "\n--- Retrieving MONARC libraries… ---\n"
 composer config -g github-oauth.github.com $GITHUB_AUTH_TOKEN
 composer install -o
 
@@ -144,7 +144,7 @@ cat > /etc/apache2/sites-enabled/000-default.conf <<EOF
     SetEnv DB_PASS $DBPASSWORD_MONARC
 </VirtualHost>
 EOF
-echo -e "\n--- Restarting Apache ---\n"
+echo -e "\n--- Restarting Apache… ---\n"
 service apache2 restart > /dev/null 2>&1
 
 
@@ -213,22 +213,22 @@ return array(
 EOF
 
 
-echo -e "\n--- Creation of the data bases---\n"
+echo -e "\n--- Creation of the data bases… ---\n"
 mysql -u $DBUSER_MONARC -p$DBPASSWORD_MONARC -e "CREATE DATABASE monarc_cli DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;" > /dev/null
 mysql -u $DBUSER_MONARC -p$DBPASSWORD_MONARC -e "CREATE DATABASE monarc_common DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;" > /dev/null
-echo -e "\n--- Populating MONARC DB ---\n"
+echo -e "\n--- Populating MONARC DB… ---\n"
 mysql -u $DBUSER_MONARC -p$DBPASSWORD_MONARC monarc_common < db-bootstrap/monarc_structure.sql > /dev/null
-mysql -u $DBUSER_MONARC -p$DBPASSWORD_MONARC monarc_common < db-bootstrap/monarc_data.sql > /dev/null 2>&1
+mysql -u $DBUSER_MONARC -p$DBPASSWORD_MONARC monarc_common < db-bootstrap/monarc_data.sql > /dev/null
 
 
-echo -e "\n--- Installation of Grunt ---\n"
+echo -e "\n--- Installation of Grunt… ---\n"
 apt-get -y install nodejs > /dev/null
 apt-get -y install npm > /dev/null
 npm install -g grunt-cli > /dev/null
-ln -s /usr/bin/nodejs /usr/bin/node
+# ln -s /usr/bin/nodejs /usr/bin/node
 
 
-echo -e "\n--- Update the project ---\n"
+echo -e "\n--- Update the project… ---\n"
 /bin/bash ./scripts/update-all.sh > /dev/null
 
 
@@ -236,7 +236,7 @@ echo -e "\n--- Create initial user and client ---\n"
 php ./vendor/robmorgan/phinx/bin/phinx seed:run -c ./module/MonarcFO/migrations/phinx.php
 
 
-echo -e "\n--- Restarting Apache ---\n"
+echo -e "\n--- Restarting Apache… ---\n"
 systemctl restart apache2.service > /dev/null
 
 
