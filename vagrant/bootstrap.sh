5c921238e46c74e6e6955c9b18884401f056bf02
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-07-17 23:36:11 +0200

    Updated address of the version checking app.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -207,7 +207,7 @@ return array(
     'appVersion' => \$package_json['version'],
 
     'checkVersion' => false,
-    'appCheckingURL' => 'https://monarc-version-checker.herokuapp.com/check/MONARC',
+    'appCheckingURL' => 'https://version.monarc.lu/check/MONARC',
 
     'email' => [
             'name' => 'MONARC',
