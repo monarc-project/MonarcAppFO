125d7c1151b06ffa29ea6c83bc7e381728ec50d6
Ruslan Baidan <ruslan.baidan@cases.lu>
2020-07-02 15:24:51 +0200

    Added the stats type test cases.

---
+++ tests/Integration/Service/StatsAnrServiceTest.php
@@ -0,0 +1,234 @@
+<?php
+
+namespace MonarcAppFo\Tests\Integration\Service;
+
+use DateTime;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
+use Laminas\ServiceManager\ServiceManager;
+use Monarc\FrontOffice\Model\Table\AnrTable;
+use Monarc\FrontOffice\Model\Table\SettingTable;
+use Monarc\FrontOffice\Stats\DataObject\StatsDataObject;
+use Monarc\FrontOffice\Stats\Exception\StatsAlreadyCollectedException;
+use Monarc\FrontOffice\Stats\Provider\StatsApiProvider;
+use Monarc\FrontOffice\Stats\Service\StatsAnrService;
+use MonarcAppFo\Tests\Integration\AbstractIntegrationTestCase;
+
+class StatsAnrServiceTest extends AbstractIntegrationTestCase
+{
+    /** @var MockHandler */
+    private $mockHandler;
+
+    /** @var array */
+    private $currentDateParams;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        static::createMyPrintTestData();
+    }
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->currentDateParams = $this->getCurrentDateParams();
+    }
+
+    protected function configureServiceManager(ServiceManager $serviceManager)
+    {
+        $serviceManager->setAllowOverride(true);
+
+        $this->mockHandler = new MockHandler();
+        $statsApiProvider = new StatsApiProvider(
+            $serviceManager->get(SettingTable::class),
+            [],
+            $this->mockHandler
+        );
+        $serviceManager->setService(StatsApiProvider::class, $statsApiProvider);
+
+        $serviceManager->setAllowOverride(false);
+    }
+
+    public function testItThrowsTheErrorWhenTheTheStatsAlreadyGeneratedForToday()
+    {
+        $this->expectException(StatsAlreadyCollectedException::class);
+        $this->expectExceptionMessage('The stats is already collected for today.');
+
+        $this->mockHandler->append(new Response(200, [], $this->getStatsResponse([
+            [
+                'type' => StatsDataObject::TYPE_RISK,
+                'data' => [
+                    'category' => 'ANR 1',
+                    'series' => [
+                        [
+                            'label' => 'Low risks',
+                            'value' => 50,
+
+                        ],
+                        [
+                            'label' => 'Medium risks',
+                            'value' => 30,
+
+                        ],
+                        [
+                            'label' => 'High risks',
+                            'value' => 10,
+
+                        ],
+                    ],
+                ],
+            ],
+        ])));
+
+        /** @var StatsAnrService $statsAnrService */
+        $statsAnrService = $this->getApplicationServiceLocator()->get(StatsAnrService::class);
+        $statsAnrService->collectStats();
+    }
+
+    public function testItDoesNotSendTheStatsWhenTheDataIsEmpty()
+    {
+        $this->mockHandler->append(new Response(200, [], $this->getStatsResponse()));
+
+        /** @var StatsAnrService $statsAnrService */
+        $statsAnrService = $this->getApplicationServiceLocator()->get(StatsAnrService::class);
+        $statsAnrService->collectStats([99, 78]);
+
+        $this->assertEquals('GET', $this->mockHandler->getLastRequest()->getMethod());
+    }
+
+    public function testItCanGenerateTheStatsForAllTheAnrs()
+    {
+        /** @var AnrTable $anrTable */
+        $anrTable = $this->getApplicationServiceLocator()->get(AnrTable::class);
+        $anrs = $anrTable->findAll();
+        $anrUuids = [];
+        foreach ($anrs as $anr) {
+            $anrUuids[] = $anr->getUuid();
+        }
+
+        $this->mockHandler->append(new Response(200, [], $this->getStatsResponse()));
+        $this->mockHandler->append(new Response(201, [], '{"status": "ok"}'));
+
+        /** @var StatsAnrService $statsAnrService */
+        $statsAnrService = $this->getApplicationServiceLocator()->get(StatsAnrService::class);
+        $statsAnrService->collectStats();
+
+        $this->assertJsonStringEqualsJsonString(
+            $this->getExpectedStatsDataJson($anrUuids),
+            $this->mockHandler->getLastRequest()->getBody()->getContents()
+        );
+    }
+
+    public function testItGenerateTheStatsOnlyForPassedAnrs()
+    {
+        $anrIdsToGenerateTheStats = [1, 2, 3];
+
+        /** @var AnrTable $anrTable */
+        $anrTable = $this->getApplicationServiceLocator()->get(AnrTable::class);
+        $anrs = $anrTable->findByIds($anrIdsToGenerateTheStats);
+        $anrUuids = [];
+        foreach ($anrs as $num => $anr) {
+            $anrUuids[] = $anr->getUuid();
+        }
+
+        $this->assertCount(\count($anrIdsToGenerateTheStats), $anrUuids);
+
+        $this->mockHandler->append(new Response(200, [], $this->getStatsResponse()));
+        $this->mockHandler->append(new Response(201, [], '{"status": "ok"}'));
+
+        /** @var StatsAnrService $statsAnrService */
+        $statsAnrService = $this->getApplicationServiceLocator()->get(StatsAnrService::class);
+        $statsAnrService->collectStats($anrIdsToGenerateTheStats);
+
+        $this->assertJsonStringEqualsJsonString(
+            $this->getExpectedStatsDataJson($anrUuids),
+            $this->mockHandler->getLastRequest()->getBody()->getContents()
+        );
+    }
+/*
+    public function testItThrowsTheExceptionIfUserIsNotLoggedIn()
+    {
+
+    }
+
+    public function testItThrowsTheExceptionIfUserDoesNotHaveTheRightsToGetTheStats()
+    {
+
+    }
+
+    public function testItAddsToTheFilterAllowedForTheUserAnrUuids()
+    {
+
+    }
+
+    public function testItAllowsToFilterBySpecificAnrsForCeoRole()
+    {
+
+    }
+
+    public function testItAllowsToGetStatsWithoutAnrsLimitationForCeoRole()
+    {
+
+    }
+
+    public function testItCanSendDifferentAggregationParamsToGetTheStats()
+    {
+
+    }
+
+    public function testItFetchesStatsForDefaultPeriodIfFromAndToDatesAreNotPassed()
+    {
+    }
+*/
+    private function getStatsResponse(array $results = []): string
+    {
+        return json_encode([
+            'metadata' => [
+                'resultset' => [
+                    'count' => \count($results),
+                    'offset' => 0,
+                    'limit' => 0,
+                ],
+            ],
+            'data' => $results,
+        ]);
+    }
+
+    private function getExpectedStatsDataJson(array $anrUuids): string
+    {
+        $statsData = json_decode(
+            file_get_contents($this->testPath . '/data/expected_stats_data_for_my_print.json'),
+            true
+        );
+
+        $expectedStats = [];
+        foreach ($anrUuids as $num => $anrUuid) {
+            foreach ($statsData as $data) {
+                $data['anr'] = $anrUuid;
+                $data['day'] = $this->currentDateParams['day'];
+                $data['week'] = $this->currentDateParams['week'];
+                $data['month'] = $this->currentDateParams['month'];
+                $data['quarter'] = $this->currentDateParams['quarter'];
+                $data['year'] = $this->currentDateParams['year'];
+                $expectedStats[] = $data;
+            }
+        }
+
+        return json_encode($expectedStats);
+    }
+
+    private function getCurrentDateParams(): array
+    {
+        $dateTime = new DateTime();
+
+        return [
+            'day' => (int)$dateTime->format('z') + 1,
+            'week' => (int)$dateTime->format('W'),
+            'month' => (int)$dateTime->format('m'),
+            'quarter' => (int)ceil($dateTime->format('m') / 3),
+            'year' => (int)$dateTime->format('Y'),
+        ];
+    }
+}
--- tests/Unit/Stats/Validator/GetStatsQueryParamsValidatorTest.php
+++ tests/Unit/Stats/Validator/GetStatsQueryParamsValidatorTest.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\EntityNotFoundException;
 use Laminas\InputFilter\InputFilter;
 use Monarc\FrontOffice\Model\Entity\Anr;
 use Monarc\FrontOffice\Model\Table\AnrTable;
+use Monarc\FrontOffice\Stats\DataObject\StatsDataObject;
 use Monarc\FrontOffice\Stats\Validator\GetStatsQueryParamsValidator;
 use Monarc\FrontOffice\Validator\FieldValidator\AnrExistenceValidator;
 use MonarcAppFo\Tests\Unit\AbstractUnitTestCase;
@@ -33,6 +34,7 @@ class GetStatsQueryParamsValidatorTest extends AbstractUnitTestCase
     public function testItIsNotValidWhenDateToLowerThenDateFromOrBiggerThenCurrentDate()
     {
         static::assertFalse($this->getStatsQueryParamsValidator->isValid([
+            'type' => StatsDataObject::TYPE_RISK,
             'dateFrom' => '2020-01-01',
             'dateTo' => '2019-12-01',
         ]));
@@ -46,6 +48,7 @@ class GetStatsQueryParamsValidatorTest extends AbstractUnitTestCase
         );
 
         static::assertFalse($this->getStatsQueryParamsValidator->isValid([
+            'type' => StatsDataObject::TYPE_RISK,
             'dateFrom' => '3020-01-01',
             'dateTo' => '3019-12-01',
         ]));
@@ -62,24 +65,26 @@ class GetStatsQueryParamsValidatorTest extends AbstractUnitTestCase
     public function testItIsValidWhenTheDatesAreTheSameOrDateFromIsLessThenDateTo()
     {
         static::assertTrue($this->getStatsQueryParamsValidator->isValid([
+            'type' => StatsDataObject::TYPE_RISK,
             'dateFrom' => '2019-12-01',
             'dateTo' => '2020-06-01',
         ]));
 
         static::assertEmpty($this->getStatsQueryParamsValidator->getErrorMessages());
         static::assertEquals(
-            ['dateFrom' => '2019-12-01', 'dateTo' => '2020-06-01', 'anrs' => []],
+            ['dateFrom' => '2019-12-01', 'dateTo' => '2020-06-01', 'anrs' => [], 'type' => StatsDataObject::TYPE_RISK],
             $this->getStatsQueryParamsValidator->getValidData()
         );
 
         static::assertTrue($this->getStatsQueryParamsValidator->isValid([
+            'type' => StatsDataObject::TYPE_RISK,
             'dateFrom' => '2019-12-01',
             'dateTo' => '2019-12-01',
         ]));
 
         static::assertEmpty($this->getStatsQueryParamsValidator->getErrorMessages());
         static::assertEquals(
-            ['dateFrom' => '2019-12-01', 'dateTo' => '2019-12-01', 'anrs' => []],
+            ['dateFrom' => '2019-12-01', 'dateTo' => '2019-12-01', 'anrs' => [], 'type' => StatsDataObject::TYPE_RISK],
             $this->getStatsQueryParamsValidator->getValidData()
         );
     }
@@ -91,12 +96,37 @@ class GetStatsQueryParamsValidatorTest extends AbstractUnitTestCase
         $this->anrTable->expects($this->at(2))->method('findById')->willThrowException(new EntityNotFoundException());
 
         static::assertFalse($this->getStatsQueryParamsValidator->isValid([
+            'type' => StatsDataObject::TYPE_RISK,
             'anrs' => [1, 2, 3, 7],
         ]));
 
         static::assertEquals(
             [
                 'anrs' => [AnrExistenceValidator::ANR_DOES_NOT_EXIST => 'Anr with the ID (3) does not exist.']
+            ],
+            $this->getStatsQueryParamsValidator->getErrorMessages()
+        );
+    }
+
+    public function testItIsNotValidWhenTypeIsNotPassedOrWrong()
+    {
+        static::assertFalse($this->getStatsQueryParamsValidator->isValid([]));
+        static::assertEquals(
+            [
+                'type' => ['isEmpty' => 'Value is required and can\'t be empty']
+            ],
+            $this->getStatsQueryParamsValidator->getErrorMessages()
+        );
+
+        static::assertFalse($this->getStatsQueryParamsValidator->isValid([
+            'type' => 'not-existed-type'
+        ]));
+        static::assertEquals(
+            [
+                'type' => [
+                    'notInArray' => 'Type should be one of the values: '
+                        . implode(', ', StatsDataObject::getAvailableTypes())
+                ]
             ],
             $this->getStatsQueryParamsValidator->getErrorMessages()
         );
