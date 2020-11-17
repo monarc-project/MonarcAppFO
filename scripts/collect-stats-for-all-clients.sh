66b5d958b16bf41255c3b145ff5dd6625a0f95df
Ruslan Baidan <ruslan.baidan@cases.lu>
2020-11-17 14:39:50 +0100

    Updated the frontoffice dependency and added the script to collects stats for all the clients on prod.

---
+++ scripts/collect-stats-for-all-clients.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+# local use:
+#for conf_file_path in /home/vagrant/monarc/config/*/local.php
+
+for conf_file_path in /var/www/*/local.php
+do
+  config_path=$(dirname "$conf_file_path")
+  export APP_CONF_DIR=$config_path
+
+  echo "[$(date)] Collecting stats for client: $config_path."
+
+  # local use:
+  #/home/vagrant/monarc/bin/console monarc:collect-stats >> "$config_path"/collect_stats.log
+
+  /var/lib/monarc/fo/bin/console monarc:collect-stats >> "$config_path"/collect_stats.log
+
+  echo "[$(date)] Finished."
+done
