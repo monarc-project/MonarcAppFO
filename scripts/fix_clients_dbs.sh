b1dd41bfa48da004f6ee45f1de031af344e5889a
Ruslan Baidan <ruslan.baidan@cases.lu>
2021-03-01 17:31:54 +0100

    Uodated the core dependency and fixed the alter anrs script.

--- scripts/fix_clients_dbs.sh
+++ scripts/fix_clients_dbs.sh
@@ -6,5 +6,6 @@ do
     db_pwd=$(grep 'password' $conf_file | head -1  | sed "s/'password' => '//" | sed "s/',//" | sed  's/^[ \t]*//;s/[ \t]*$//')
     echo "Alter anrs table raw size for $db_user ..."
     mysql -u $db_user -p$db_pwd $db_user -e "ALTER TABLE anrs ROW_FORMAT=DYNAMIC;"
+    mysql -u $db_user -p$db_pwd $db_user -e "OPTIMIZE TABLE anrs;"
     echo "Done."
 done
