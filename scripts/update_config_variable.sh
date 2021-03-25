e54a66088b505a217e2b8549bddaef815ee02dc0
Ruslan Baidan <ruslan.baidan@cases.lu>
2021-03-25 15:21:46 +0100

    Remvoed the part of updating the clients configs.

--- scripts/update_config_variable.sh
+++ scripts/update_config_variable.sh
@@ -1,14 +1,9 @@
 version=$(cat ./VERSION.json)
 version_affected='{"major":2, "minor":10, "hotfix":3}'
 if [[ $version == "$version_affected" ]]; then
-  for conf_file in /var/www/*/local.php
-  do
-    if [ -f "$conf_file" ]; then
-      sed -i 's/https:\/\/objects.monarc.lu\/api\/v1\//https:\/\/objects.monarc.lu\/api\//g' "$conf_file"
-      echo "Done for $conf_file."
-    else
-      sed -i 's/https:\/\/objects.monarc.lu\/api\/v1\//https:\/\/objects.monarc.lu\/api\//g' "./config/autoload/local.php"
-      echo "Done for ./config/autoload/local.php"
-    fi
-  done
+  $conf_file='./config/autoload/local.php'
+  if [ -f "$conf_file" ]; then
+    sed -i 's/https:\/\/objects.monarc.lu\/api\/v1\//https:\/\/objects.monarc.lu\/api\//g' "$conf_file"
+    echo "Done for $conf_file"
+  fi
 fi
