22fb2fe28d4f79f34e3a8e7568fe22d9511eb4d7
Ruslan Baidan <ruslan.baidan@cases.lu>
2021-04-23 11:20:57 +0200

    Renamed the script.

--- scripts/update_config_variable.sh
+++
@@ -1,9 +0,0 @@
-version=$(cat ./VERSION.json)
-version_affected='{"major":2, "minor":10, "hotfix":3}'
-if [[ $version == "$version_affected" ]]; then
-  $conf_file='./config/autoload/local.php'
-  if [ -f "$conf_file" ]; then
-    sed -i 's/https:\/\/objects.monarc.lu\/api\/v1\//https:\/\/objects.monarc.lu\/api\//g' "$conf_file"
-    echo "Done for $conf_file"
-  fi
-fi
