928cf04cb23e6cd3ac7be8a7408b49f84dfd0e91
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-05-06 15:14:30 +0200

    chg: [config] selects the value of the configuration to change.

--- scripts/update_config_variables.sh
+++ scripts/update_config_variables.sh
@@ -2,6 +2,6 @@
 
 conf_file='./config/autoload/local.php'
 if [ -f "$conf_file" ]; then
-  sed -E -i "s/(.*) => (.*)\/v1/\1 => \2/g" "$conf_file"
+  sed -E -i "s/'mospApiUrl' => (.*)\/v1/'mospApiUrl' => \1/g" "$conf_file"
   echo "Done for $conf_file"
 fi
