30a0414fe2d8deb6f7a9382f580324a67b523aaf
Ruslan Baidan <ruslan.baidan@cases.lu>
2022-03-09 11:25:15 +0100

    Fixed the namespace load case.

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,7 +11,7 @@
  * file.
  */
 
-use Doctrine\DBAL\Driver\PDO\MySql\Driver;
+use Doctrine\DBAL\Driver\PDO\MySQL\Driver;
 use Monarc\Core\Service\DoctrineCacheServiceFactory;
 use Monarc\Core\Service\DoctrineLoggerFactory;
 
