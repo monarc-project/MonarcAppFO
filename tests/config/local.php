e9ecab88dc47d392b7480c361d72a231ab5e7b49
Ruslan Baidan <ruslan.baidan@nc3.lu>
2026-08-10 20:34:21 +0200

    Alligned the languages configuration between BO and FO, UI and data models.

--- tests/config/local.php
+++ tests/config/local.php
@@ -72,9 +72,21 @@ return [
         ],
     ],
 
-    'activeLanguages' => ['fr', 'en', 'de', 'nl',],
+    'defaultLanguageIndex' => 1,
 
-    'appVersion' => '3.0.0',
+     // Languages available for the analysis / DB data.
+    'languages' => [
+        'fr' => ['index' => 1, 'label' => 'Français'],
+        'en' => ['index' => 2, 'label' => 'English'],
+        'de' => ['index' => 3, 'label' => 'Deutsch'],
+        'nl' => ['index' => 4, 'label' => 'Dutch'],
+    ],
+
+    // Languages available for the user interface.
+    'activeLanguages' => ['fr', 'en', 'de', 'nl', 'es', 'ro', 'it', 'ja', 'pl', 'pt', 'zh'],
+
+
+    'appVersion' => '2.14.1',
 
     'checkVersion' => false,
     'appCheckingURL' => 'https://version.monarc.lu/check/MONARC',
